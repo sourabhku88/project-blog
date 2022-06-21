d9d6cfaf18353df2d459cbe01c1dce86d24af09a
Sourabh kumar <sourabhkumar811307@gmail.com>
2022-06-21 15:45:05 +0530

    add create blog

--- src/controller/blogController/blogController.js
+++ src/controller/blogController/blogController.js
@@ -1,1 +1,35 @@
-sdfsihod
+const blogModel = require('../blogController/blogController')
+
+// CREATE BLOG
+const createBlog =  async (req,res)=>{
+    try{
+       if(Object.keys(req.body).length < 0 ) return res.status(400).send({status: false, msg:"fill all fields"})
+
+    //    const user = await 
+    }
+    catch(err){
+        console.log(err.message);
+        res.status(500).send({status: false, msg:err.message})
+    }
+}
+
+// GET BLOGS
+const getBlogs = async (req,res)=>{
+    try{
+
+    }
+    catch(err) {res.status(500).send({status: false, msg:err.message})}
+}
+
+// GET BLOGS
+const updateBlog = async (req,res)=>{
+    try{
+        // const 
+    }
+    catch(err){res.status(500).send({status: false, msg:err.message})
+    }
+}
+
+module.exports.createBlog = createBlog;
+module.exports.getBlogs = getBlogs;
+module.exports.updateBlog = updateBlog;
--- src/router/rout.js
+++ src/router/rout.js
@@ -1,10 +1,25 @@
 const express =require('express');
+const { createBlog, getBlogs } = require('../controller/blogController/blogController');
+const { blogDeleteCheck } = require('../middleWares/deleteCheck');
 const router =express.Router();
 
 
 router.get('/',(req,res)=>{
     res.status(200).send({msg:"work"})
 })
+
+
+router.post('/authors', )
+
+router.post('/blogs', createBlog)
+
+router.get('/blogs',blogDeleteCheck , getBlogs )
+
+router.put('/blogs/:blogId', blogDeleteCheck , )
+
+router.delete('/blogs',  )
+
+router.delete('/blogs/:blogId',  )
 
 
 module.exports = router;
