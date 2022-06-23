b6a64e474aec856d12416072de96427c73da664f
ShivanandKasture <shivanandkasture1415@gmail.com>
2022-06-23 13:52:12 +0530

    auth

--- src/middleWares/auth.js
+++ src/middleWares/auth.js
@@ -1,18 +1,52 @@
 const jwt = require('jsonwebtoken');
+const blogModel = require("../../model/blogModel");
+const { isVaildObjectId } = require("mongoose");
 
-const authentication = async (req,res , next)=>{
-    try{
+// Authentication 
+const authentication = async (req, res, next) => {
+    try {
         const token = req.headers["x-api-key"];
-        if(!token) return res.status(401).send({status:false,msg:"user has no token" });
+        if (!token) return res.status(401).send({ status: false, msg: "user has no token" });
 
-        jwt.verify(token , "ROOM 26(shubhra,shivanand,sourabh,shiv)/blog-project-1");
+        jwt.verify(token, "ROOM 26(shubhra,shivanand,sourabh,shiv)/blog-project-1");
 
         next()
 
-    }catch(error) {return res.status(500).send({status:false,msg:error.message })}
+    } catch (error) { return res.status(500).send({ status: false, msg: error.message }) }
+}
+
+//Authorisation
+const authorisation = async (req, res, next) => {
+
+    try {
+        let authorid = req.params.authorId;
+
+        let token = req.headers["x-auth-token"];
+
+        if (!authorid) return res.status(400).send({ status: false, msg: "Please enter authorId." });
+
+        if (!isVaildObjectId(authorid)) return res.status(400).send({ status: false, msg: "Please enter vaild authorId." });
+
+        let userDetails = await blogModel.findById(authorid);
+
+        if (!userDetails) return res.status(404).send({ status: false, msg: 'No such user exists' })
+
+        const tokenDecoded = jwt.verify(token, "ROOM 26(shubhra,shivanand,sourabh,shiv)/blog-project-1");
+
+        if (userDetails._id != tokenDecoded._id) return res.status(403).send({ status: false, msg: "User logged is not allowed to modify the requested users data" })
+
+        next();
+
+    } catch (error) {
+
+        res.status(500).send({ status: false, msg: error.message });
+    }
+
+
+
 }
 
 
 
-
 module.exports = authentication
+module.exports.authorisation = authorisation;
--- src/router/rout.js
+++ src/router/rout.js
@@ -1,31 +1,27 @@
 const express =require('express');
 const { createAuthor,loginAuthor} = require('../controller/authorController/authorController');
 const { createBlog, getBlogs ,updateBlog ,deleteBlog ,deleteBlogByAny} = require('../controller/blogController/blogController');
-const authentication = require('../middleWares/auth');
+const {authentication, authorisation} = require('../middleWares/auth');
 
 
 const { blogDeleteCheck } = require('../middleWares/deleteCheck');
 const router =express.Router();
 
-const lognUser = require("../controller/userController/loginUser");
-
-
 router.post('/authors', createAuthor)
 
 router.post('/author/login', loginAuthor)
 
-router.post('/blogs', createBlog)
+router.post('/blogs', authentication,authorisation, createBlog)
 
-router.get('/blogs', authentication, getBlogs ) 
+router.get('/blogs', authentication,authorisation, getBlogs ) 
 
-router.put('/blogs/:blogId', blogDeleteCheck ,updateBlog )
+router.put('/blogs/:blogId',authentication, authorisation,blogDeleteCheck ,updateBlog )
 
-router.delete('/blogs', deleteBlogByAny )
+router.delete('/blogs',authentication, authorisation, deleteBlogByAny )
 
-router.delete('/blogs/:blogId', deleteBlog )
+router.delete('/blogs/:blogId',authentication, authorisation,blogDeleteCheck, deleteBlog )
 
 
-router.post('/login', lognUser.lognUser);
 
 
 module.exports = router;
